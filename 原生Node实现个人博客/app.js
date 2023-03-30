1caccea07cd12060cc5684f0a7e634eeddc9028d
yunmu <1416900346@qq.com>
2023-03-30 11:05:47 +0800

    解析 cookie ，使用 cookie 鉴权登录

--- 原生Node实现个人博客/app.js
+++ 原生Node实现个人博客/app.js
@@ -3,64 +3,82 @@ const handleUserRouter = require('./src/router/user')
 const handleBlogRouter = require('./src/router/blog')
 
 const serverHandle = (req, res) => {
-	// 设置返回格式为 json
-	res.setHeader('Content-type', 'application/json')
+  // 设置返回格式为 json
+  res.setHeader('Content-type', 'application/json')
 
-	// 解析 path
-	req.path = req.url.split('?')[0]
+  // 解析 path
+  req.path = req.url.split('?')[0]
 
-	// 解析 query
-	req.query = queryString.parse(req.url.split('?')[1])
+  // 解析 query
+  req.query = queryString.parse(req.url.split('?')[1])
 
-	// 获取请求体内容
-	getPostData(req).then(postData => {
-		req.body = postData
+  req.cookie = parseCookie(req.headers.cookie)
 
-		// 登录路由
-		const userData = handleUserRouter(req, res)
-		if (userData) {
-			res.end(JSON.stringify(userData))
-			return
-		}
+  // 获取请求体内容
+  getPostData(req).then(postData => {
+    req.body = postData
 
-		// 博客路由
-		const blogData = handleBlogRouter(req, res)
-		if (blogData) {
-			res.end(JSON.stringify(blogData))
-			return
-		}
+    // 登录路由
+    const userData = handleUserRouter(req, res)
+    if (userData) {
+      res.end(JSON.stringify(userData))
+      return
+    }
 
-		// 未命中路由
-		res.writeHead(404, { 'Content-type': 'text/plain' })
-		res.write('404 Not Found')
-		res.end()
-	})
+    // 博客路由
+    const blogData = handleBlogRouter(req, res)
+    if (blogData) {
+      res.end(JSON.stringify(blogData))
+      return
+    }
+
+    // 未命中路由
+    res.writeHead(404, { 'Content-type': 'text/plain' })
+    res.write('404 Not Found')
+    res.end()
+  })
 }
 
 function getPostData(req) {
-	return new Promise(resolve => {
-		if (req.method !== 'POST') {
-			resolve({})
-			return
-		}
-		if (req.headers['content-type'] !== 'application/json') {
-			resolve({})
-			return
-		}
+  return new Promise(resolve => {
+    if (req.method !== 'POST') {
+      return resolve({})
+    }
+    if (req.headers['content-type'] !== 'application/json') {
+      return resolve({})
+    }
 
-		let postData = ''
-		req.on('data', chunk => {
-			postData += chunk.toString()
-		})
-		req.on('end', () => {
-			if (!postData) {
-				resolve({})
-				return
-			}
+    let postData = ''
 
-			resolve(JSON.parse(postData))
-		})
-	})
+    req.on('data', chunk => {
+      postData += chunk.toString()
+    })
+
+    req.on('end', () => {
+      if (!postData) {
+        return resolve({})
+      }
+
+      resolve(JSON.parse(postData))
+    })
+  })
+}
+
+function parseCookie(cookie = '') {
+  const cookieObj = Object.create(null)
+
+  cookie.split(';').forEach(item => {
+    if (!item) {
+      return
+    }
+
+    const str = item.split('=')
+    const key = str[0].trim()
+    const val = str[1]
+    cookieObj[key] = val
+  })
+
+  return cookieObj
 }
 
 module.exports = serverHandle
--- 原生Node实现个人博客/src/router/user.js
+++ 原生Node实现个人博客/src/router/user.js
@@ -5,18 +5,37 @@ const handleUserRouter = (req, res) => {
   const method = req.method
   const path = req.path
 
+  if (method === 'GET' && req.path === '/api/user/logintest') {
+    if (req.cookie.username) {
+      return Promise.resolve(new SuccessModel())
+    }
+    return Promise.resolve(new ErrorModel('登录失败'))
+  }
+
   if (method === 'POST' && path === '/api/blog/login') {
     const { username, password } = req.body
     const result = loginCheck(username, password)
 
     return result.then(val => {
-      if (val) {
+      if (val.username) {
+        // 操作 cookie
+        res.setHeader(
+          'Set-Cookie',
+          `username=${data.username}; path='/'; httpOnly; expires=${getCookieExpires()}`
+        )
         return new SuccessModel()
       } else {
         return new ErrorModel('登录失败')
       }
     })
   }
+}
+
+// 获取 cookie 过期时间
+function getCookieExpires() {
+  const d = new Date()
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000)
+  return d.toGMTString()
 }
 
 module.exports = handleUserRouter
